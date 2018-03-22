53660c9e88fbf4bfaf78120af6833a695c27b5cf
lmouillet <lmouillet@gingalab.com>
2018-03-22 15:32:47 +0100

    merge of both branches

--- client.py
+++ client.py
@@ -17,11 +17,11 @@ class P2pChat(tk.Frame):
 
         master.wm_title("test name")
         tk.Frame.__init__(self, master)
-        # self.grid()
         self.pack(fill=tk.BOTH, expand=1)
         master.geometry(str(self.width) + "x" + str(self.height))
         self.create_mainmenu()
-        self.client = Client()
+        # self.user_list_window()
+        self.client = Client(self)
 
     def create_mainmenu(self):
         menubar = tk.Menu(self)
@@ -36,51 +36,6 @@ class P2pChat(tk.Frame):
         menu.add_command(label="Exit", command=self.close_app)
         menubar.add_cascade(label="Menu", menu=menu)
         self.master.config(menu=menubar)
-
-        # # create all the main frames
-        # toolbar_frame = tk.Frame(self, relief="sunken", width=self.width,
-        #                          height=(self.height / 10), padx=5,
-        #                          bg="red")
-        # middle_frame = tk.Frame(self, width=self.width,
-        #                         height=(8 * (self.height / 10)), padx=5,
-        #                         bg="green")
-        # footer_frame = tk.Frame(self, width=self.width,
-        #                         height=(self.height / 10), padx=5,
-        #                         bg="yellow")
-
-        # # sets the weight of the main window
-        # self.master.grid_rowconfigure(1, weight=1)
-        # self.master.grid_columnconfigure(0, weight=1)
-
-        # # grids the frames
-        # toolbar_frame.grid(row=0, sticky="ew")
-        # middle_frame.grid(row=1, sticky="nesw")
-        # footer_frame.grid(row=2, sticky="ew")
-
-        # toolbar_button_connect = tk.Button(toolbar_frame, text="Cth",
-        #                                    bg="white", bd=0, padx=10)
-        # toolbar_button_username = tk.Button(toolbar_frame, text="Cun",
-        #                                     bg="white", bd=0, padx=10)
-        # toolbar_label_username = tk.Label(toolbar_frame, text="cun",
-        #                                   bg="blue")
-        # toolbar_button_connect.grid(row=0, column=0, columnspan=3, sticky="e")
-        # toolbar_button_username.grid(row=0, column=4, columnspan=3, sticky="e")
-        # toolbar_label_username.grid(row=0, column=7)
-
-        # toolbar_frame.grid(row=0, column=0, columnspan=10)
-        # connect_to_host_button = tk.Button(toolbar_frame, relief="groove")
-        # connect_to_host_button["text"] = "cth"
-        # connect_to_host_button.grid(row=0, column=0, sticky="nesw")
-        # get_room_list_button = tk.Button(toolbar_frame, relief="groove")
-        # get_room_list_button["text"] = "grl"
-        # get_room_list_button.grid(row=0, column=1, sticky="nesw")
-        # change_username_button = tk.Button(toolbar_frame, relief="groove")
-        # change_username_button["text"] = "cun"
-        # change_username_button.grid(row=0, column=2, sticky="nesw")
-        # username_label_frame = tk.LabelFrame(toolbar_frame, text="UserName", bg="blue")
-        # username_label_frame.grid(row=0, column=5)
-        # username_label = tk.Label(text="UserName")
-        # username_label.grid(row=0, column=5)
 
         msg_frame = tk.Frame(self)
         msg_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
@@ -114,10 +69,18 @@ class P2pChat(tk.Frame):
     def close_window_and_call_function(self, args, window, function):
         window.destroy()
 
+    def user_list_window(self, users):
+        user_list_window = tk.Toplevel(root)
+        for user in users:
+            user_label = tk.Label(user_list_window, text=user.name)
+            user_label.pack(side=tk.TOP, fill=tk.X)
+        print("user_list_window")
 
     def room_list_window(self):
-        table = [{"name": "room1"}, {"name": "room2"}, {"name": "room3"},
-                 {"name": "room4"}]
+        self.client.send_data(self.client.s, "gethostlist", ['null'], )
+
+    def room_list(self, table):
+        # table = [{"name": "room1"}, {"name": "room2"}, {"name": "room3"},{"name": "room4"}]
         room_list_window = tk.Toplevel(root)
         room_list_frame = tk.Frame(room_list_window)
         room_list_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
@@ -168,17 +131,20 @@ class P2pChat(tk.Frame):
         print("changing username")
 
     def get_chat_list(self):
-        self.client.s.send("/gethostlist".encode())
+        self.client.send_data(self.client.s, "gethostlist", ['null'])
         print("getting chat list")
 
-    def send_message_to_chat(self, event):
+    def send_message_to_chat(self):
         print("sending message to chat")
         msg = self.msg_entry.get()
-        print(msg)
         self.msg_entry.delete(0, tk.END)
-        self.client.s.send(msg.encode())
+        self.client.send_message_user('msg', msg)
 
-    def update_message_list(self):
+    def update_message_list(self, msg):
+        self.msg_window.config(state=tk.NORMAL)
+        self.msg_window.insert(tk.END, "%s\n" % msg)
+        self.msg_window.yview(tk.END)
+        self.msg_window.config(state=tk.DISABLED)
         print("updating the message_list")
 
     def start_hosting(self):
@@ -187,62 +153,92 @@ class P2pChat(tk.Frame):
 
 
 class Client:
+    users = []
+    server = ('127.0.0.1', 10042)
+    p2pclient = []
 
-    server = ('127.0.0.1', 10000)
-
-    def __init__(self):
-        # port = 10001
-        # print("heightllo")
+    def __init__(self, p2p_chat):
+        self.p2p_chat = p2p_chat
+        print("init")
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # ip = socket.gethostbyname(socket.gethostname())
-        # while True:
-            # try:
-                # self.s.bind((ip, port))
-                # break
-            # except:
-                # port += 1
-        # self.s.connect(self.server)
-        # ths = threading.Thread(target=self.recv_handler_server)
-        # ths.start()
+        self.su = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ip = socket.gethostbyname(socket.gethostname())
+
+        self.s.bind(('', 0))
+        self.su.bind(('', 0))
+        self.s.connect(self.server)
+        self.send_data(self.s, "connexion", self.su.getsockname())
+        ths1 = threading.Thread(target=self.recv_handler_server)
+        ths2 = threading.Thread(target=self.handle_user_connection)
+        ths1.start()
+        ths2.start()
         # lc = threading.Thread(target=self.listen_connection)
         # lc.start()
 
-    def connect_to_chat(self, ip, port):
-        print(ip)
-        print(port)
 
     def listen_connection(self):
+        print("listen")
         while True:
             # self.s.listen(5)
             sock, addr = self.s.accept()
-            if addr[0] == "127.0.0.1" & addr[1] == 10000:
+            if addr[0] == "127.0.0.1" & addr[1] == server[1]:
                 ths = threading.Thread(target=self.recv_handler_server,
                                        kwargs={'sock': sock})
                 ths.start()
             else:
+                print("user reac_handler")
                 th = threading.Thread(target=self.recv_handler,
                                       kwargs={'sock': sock})
                 th.start()
         self.s.close()
 
     def recv_handler_server(self):
+
         while True:
-            msg = self.s.recv(1024)
-            msg = msg.decode("utf-8")
+            msg = self.s.recv(1024).decode("utf-8")
             temp = json.loads(msg)
-            print(str(temp))
             if msg:
-                print("message from server")
-                print(msg)
+                print("message from server test")
+                for user in temp:
+                    if user[1] != self.su.getsockname()[1]:
+                        self.p2pclient.append(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+                        self.p2pclient[-1].bind(('', 0))
+                        print(str(user[0]), user[1])
+                        self.p2pclient[-1].connect((user[0], user[1]))
+                        th = threading.Thread(target=self.recv_handler, kwargs={'sock': self.p2pclient[-1]})
 
     def recv_handler(self, sock):
+        print("recv_handler")
+        print("message is not from user")
         while True:
-            msg = sock.recv(1024)
-            msg.decode()
-            if not msg:
-                print("message is not from server")
-                print(msg)
+            msg = sock.recv(1024).decode()
+            if not msg: break
+            self.p2p_chat.update_message_list(str(msg))
 
+
+    def handle_user_connection(self):
+        print("handle_user_connection")
+        while True:
+            self.su.listen(5)
+            sock, addr = self.su.accept()
+            acu = threading.Thread(target=self.add_connected_user,
+                                   kwargs=({"sock": sock, "addr": addr}))
+            acu.start()
+            print('Connected with ' + addr[0] + ':' + str(addr[1]))
+        self.s.close()
+
+    def send_data(self, sock, type_message, data):
+        sock.send(str("{\"message\": \""+str(type_message)+"\", \"data\": "+json.dumps(data)+"}").encode("utf-8"))
+
+    def add_connected_user(self, sock, addr):
+        print(sock, addr)
+        new_user = [sock, addr[0], addr[1]]
+        self.users.append(new_user)
+        th = threading.Thread(target=self.recv_handler, kwargs={'sock': sock})
+        th.start()
+    def send_message_user(self, type_message, data):
+        for user in self.p2pclient:
+            user.send(data.encode())
 
 class Chat:
 
